fa1d25144459ae7263214f224ae011a7d442d7cd
Alex Forrana <forrana@gmail.com>
2021-04-30 00:08:41 +0300

    feat: add House, Room, Plant models to admin panel

--- app/plants/admin.py
+++ app/plants/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Plant, Room, House
+
+
+admin.site.register(House)
+admin.site.register(Room)
+admin.site.register(Plant)
