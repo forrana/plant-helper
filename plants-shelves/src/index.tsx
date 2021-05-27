ac1180352d2ff0c736b623f657d45f5c16d33088
Alex Forrana <forrana@gmail.com>
2021-05-27 23:34:27 +0300

    feat: add bootstrap and reactstrap styles with components

--- plants-shelves/src/index.tsx
+++ plants-shelves/src/index.tsx
@@ -8,6 +8,7 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
