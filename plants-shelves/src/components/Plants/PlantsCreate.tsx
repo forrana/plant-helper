d8dffcae425e40d99d436da93ff229a06097d9df
Alex Forrana <forrana@gmail.com>
2021-05-27 23:35:40 +0300

    refactor: PlantsCreate - use state hooks, rewrite to reactstrap components

--- plants-shelves/src/components/Plants/PlantsCreate.tsx
+++ plants-shelves/src/components/Plants/PlantsCreate.tsx
@@ -1,39 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_PLANT } from '../queries'
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Redirect } from "react-router-dom";
 
 function PlantsCreate() {
-  let nameInput: HTMLInputElement | null, scientificNameInput: HTMLInputElement | null;
+  const [submitted, setSubmitted] = useState(false);
+  const [plantName, setPlantName] = useState("");
+  const [scientificName, setScientificName] = useState("");
+
   const [addPlant, { data }] = useMutation(ADD_PLANT);
 
-    return (
-      <form onSubmit={e => {
-          e.preventDefault();
-          if(nameInput && scientificNameInput) {
-            let name: string = nameInput.value;
-            let scientificName: string = scientificNameInput.value;
-            addPlant({ variables: { plantName: name, scientificName: scientificName } });
-            nameInput.value = "";
-            scientificNameInput.value = "";
-          }
+  const handlePlantNameInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.persist();
+    setPlantName(event.target.value);
+  };
 
-        }}
-      >
-        <label>
-          Name:
-          <input type="text" name="name" ref={node => {
-            nameInput = node;
-          }}/>
-        </label>
-        <label>
-          Scientific name:
-          <input type="text" name="scientificName" ref={node => {
-            scientificNameInput = node;
-          }}/>
-        </label>
-        <input type="submit" value="Add plant" />
-      </form>
-    )
+  const handleScientificNameInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.persist();
+    setScientificName(event.target.value);
+  };
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if(plantName && scientificName) {
+      addPlant({ variables: { plantName, scientificName } })
+      setPlantName("");
+      setScientificName("");
+      setSubmitted(true)
+    }
+  }
+
+  if (submitted) {
+    return <Redirect push to="/"/>
+  }
+
+  return (
+    <Form
+      onSubmit={handleFormSubmit}
+    >
+      <FormGroup>
+        <Label for="name">Name:</Label>
+        <Input type="text" name="name" id="name" placeholder="Plant name"
+          value={plantName}
+          onChange={handlePlantNameInputChange}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="scientificName">Scientific name:</Label>
+        <Input type="text" name="scientificName" id="scientificName" placeholder="Scientific name"
+          value={scientificName}
+          onChange={handleScientificNameInputChange}
+        />
+      </FormGroup>
+      <Button type="submit">Add plant</Button>
+    </Form>
+  )
 }
 
 export { PlantsCreate }
