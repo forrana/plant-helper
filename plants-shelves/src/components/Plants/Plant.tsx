7a6f820e528157fc28fd8f56396541cd56baa141
Alex Forrana <forrana@gmail.com>
2021-05-28 23:51:30 +0300

    feat: add placeholder for plant picture

--- plants-shelves/src/components/Plants/Plant.tsx
+++ plants-shelves/src/components/Plants/Plant.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { PlantType } from './models'
-import { Card, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+import { Card, CardText, CardBody, CardTitle, CardSubtitle, CardImg } from 'reactstrap';
 import styles from "./Plant.module.css"
 
 type PlantProps = {
@@ -11,6 +11,7 @@ function Plant({ plant }: PlantProps) {
     return (
       <Card className={styles.plant}>
         <CardBody>
+          <div className={styles.image}>&#129716;</div>
           <CardTitle tag="h5">{plant.name}</CardTitle>
           <CardSubtitle tag="h6" className="mb-2 text-muted">{plant.scientificName}</CardSubtitle>
           <CardText>Water in {plant.daysUntilNextWatering} day(s).</CardText>
