8fdc0dc85687a58cc489d292e1767dbdec0ea168
Alex Forrana <forrana@gmail.com>
2021-07-02 23:34:04 +0300

    refactor: Move plants related code to PlantsContainer

--- plants-shelves/src/App.tsx
+++ plants-shelves/src/App.tsx
@@ -1,50 +1,17 @@
 import React, { useReducer, Dispatch } from 'react';
-import { useHistory } from "react-router-dom";
 import {
   Switch,
   Route
 } from "react-router-dom";
-import { useQuery } from '@apollo/client';
-import { Button } from 'reactstrap';
-
+import PlantsContainer from './components/Plants/PlantsContainer'
 import styles from './App.module.css';
-import { PlantsList } from './components/Plants/PlantsList'
 import { PlantsCreate } from './components/Plants/PlantsCreate'
 import { GlobalReducerAction, GlobalState } from './components/Plants/models'
 import { globalReducer, initialGlobalState } from './components/Plants/GlobalReducer'
 import PlantsDispatch from './components/Plants/PlantsDispatch'
-import { PlantsData } from './components/Plants/models'
-
-import { GET_ALL_PLANTS } from './components/Plants/queries'
-
 
 function App() {
   const [state, dispatch]:[GlobalState, Dispatch<GlobalReducerAction>] = useReducer(globalReducer, initialGlobalState);
-
-  const { loading, data, error } = useQuery<PlantsData>(
-    GET_ALL_PLANTS,
-    {
-      onCompleted: (data: PlantsData) => {
-        dispatch({ type: 'load', plants: data.plants })
-      }
-    }
-  );
-
-  const history = useHistory();
-  const goToCreatePage = () => history.push("/create");
-
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :( {error.message}</p>;
-
-  if (data?.plants?.length === 0) return (
-    <section>
-      <p> No plants yet, create the first one! </p>
-      <Button onClick={goToCreatePage} outline color="primary" title="Add new plant">
-        Create!
-      </Button>
-    </section>
-  )
 
   return (
     <div className={styles.App}>
@@ -52,7 +19,7 @@ function App() {
         <Switch>
           <Route path="/create" component={PlantsCreate}/>
           <Route path="/">
-            <PlantsList plants={state.plants} />
+            <PlantsContainer state={state}/>
           </Route>
         </Switch>
       </PlantsDispatch.Provider>
---
+++ plants-shelves/src/components/Plants/PlantsContainer.tsx
@@ -0,0 +1,50 @@
+import React, { useContext } from 'react';
+import { useHistory } from "react-router-dom";
+import { Button } from 'reactstrap';
+import { PlantsList } from './PlantsList'
+import { GET_ALL_PLANTS } from './queries'
+import { useQuery } from '@apollo/client';
+import PlantsDispatch from './PlantsDispatch'
+import { PlantsData } from './models'
+import { GlobalState } from './models'
+
+interface PlantsContainerProps {
+  state: GlobalState
+}
+
+function PlantsContainer(props: PlantsContainerProps) {
+  const dispatch = useContext(PlantsDispatch);
+
+  const { loading, data, error } = useQuery<PlantsData>(
+    GET_ALL_PLANTS,
+    {
+      onCompleted: (data: PlantsData) => {
+        dispatch && dispatch({ type: 'load', plants: data.plants })
+      }
+    }
+  );
+
+  const history = useHistory();
+  const goToCreatePage = () => history.push("/create");
+
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :( {error.message}</p>;
+
+  if (data?.plants?.length === 0) return (
+    <section>
+      <p> No plants yet, create the first one! </p>
+      <Button onClick={goToCreatePage} outline color="primary" title="Add new plant">
+        Create!
+      </Button>
+    </section>
+  )
+
+  return (
+    <PlantsList plants={props.state.plants} />
+  )
+
+
+}
+
+export default PlantsContainer
