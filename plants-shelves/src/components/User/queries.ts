b5c6c5e2e815ecff2958b0069972f3e365d9f0d3
Alex Forrana <forrana@gmail.com>
2021-08-23 23:31:02 +0300

    fix: remove token and request token from register mutation response

--- plants-shelves/src/components/User/queries.ts
+++ plants-shelves/src/components/User/queries.ts
@@ -4,9 +4,7 @@ const CREATE_USER = gql`
   mutation Register($email: String!, $username: String!, $password1: String!, $password2: String!) {
     register(email: $email, username: $username, password1: $password1, password2: $password2) {
       success,
-      errors,
-      token,
-      refreshToken
+      errors
     }
   }
 `;
