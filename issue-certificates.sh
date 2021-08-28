a7a6b336c942c48981341b5cb33806a6306d04a9
Alex Forrana <forrana@gmail.com>
2021-08-28 21:31:43 +0300

    feat: change domain name to plants.ml

--- issue-certificates.sh
+++ issue-certificates.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-docker exec -it plant-helper_web_1 certbot --nginx -n -d plast.ml --email plant.assistant.ml@gmail.com --agree-tos
-
+docker exec -it plant-helper_web_1 certbot --nginx -n -d plants.ml --email plant.assistant.ml@gmail.com --agree-tos
