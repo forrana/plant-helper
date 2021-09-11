8edbc97386c617de4b7b5022c10d8aacd5e2063f
Alex Forrana <forrana@gmail.com>
2021-09-11 22:42:39 +0300

    feat: Push Notifications - open app on notification click

--- plants-shelves/src/service-worker.ts
+++ plants-shelves/src/service-worker.ts
@@ -97,4 +97,29 @@ const showLocalNotification = (title: string, body: string, swRegistration: any)
   swRegistration.showNotification(title, options)
 }
 
+self.addEventListener('notificationclick', function(event) {
+  console.log('On notification click: ', event.notification.tag);
+  // Android doesn't close the notification when you click on it
+  // See: http://crbug.com/463146
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(
+    /* tslint:disable-next-line */
+    self.clients.matchAll({
+      type: "window"
+    })
+    .then(function(clientList: any) {
+      for (var i = 0; i < clientList.length; i++) {
+        var client = clientList[i];
+        if (client.url == '/' && 'focus' in client)
+          return client.focus();
+      }
+      if (self.clients.openWindow) {
+        return self.clients.openWindow('/');
+      }
+    })
+  );
+});
 // Any other custom service worker logic can go here.
