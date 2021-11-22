4c228b4d21ced44ecdda4bc0ac1bfe2987bacb60
Alex Forrana <forrana@gmail.com>
2021-11-22 22:42:47 +0200

    feat: NavBar apply proper styles, hide logout button into collapse element

--- plants-shelves/src/components/Plants/PlantsNavBar.tsx
+++ plants-shelves/src/components/Plants/PlantsNavBar.tsx
@@ -7,30 +7,42 @@ import {
     NavItem,
     NavLink,
     NavbarText,
+    NavbarToggler,
+    Collapse,
   } from 'reactstrap';
 import UserContext from '../User/UserContext'
 import uiStyles from '../UI/UIElements.module.css'
-import styles from './PlantsNavBar.module.css'
 import CreateModal from './CreateModal';
 
 const PlantsNavBar = () => {
   const userContext = useContext(UserContext);
   const [modal, setModal] = useState(false);
+  const [menu, setMenu] = useState(false);
   const toggleModal = () => setModal(!modal);
+  const toggleMenu = () => setMenu(!menu)
 
   return (
     <>
-      <Navbar color="light" light expand="md" className={styles.navbarNarrow}>
+      <Navbar color="light" light expand="md" container="sm" fixed="top">
         <NavbarBrand><img src="icon-192.png" alt="app logo"/></NavbarBrand>
-        <Nav className="mr-auto" navbar>
+        <Nav navbar>
             <NavItem>
               <Button onClick={toggleModal} data-testid="create-btn" outline className={uiStyles.roundButton} color="primary" title="Add new plant">
                 &#10133;
               </Button>
             </NavItem>
           </Nav>
-        <NavbarText>Welcome <b>{ userContext.username }</b></NavbarText>
-        <NavLink href="/logout">Logout</NavLink>
+        <NavbarText>Hi, <b>{ userContext.username }</b></NavbarText>
+        <NavbarToggler
+          className="me-2"
+          onClick={toggleMenu}
+        />
+        <Collapse
+          isOpen={menu}
+          navbar
+        >
+          <NavLink active href="/logout">Logout</NavLink>
+        </Collapse>
       </Navbar>
       <CreateModal isOpen={modal} toggleAction={toggleModal} />
     </>
