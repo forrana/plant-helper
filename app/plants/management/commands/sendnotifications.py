60e68b22a51335fa5c6592f3f85eff97cfc140d1
Alex Forrana <forrana@gmail.com>
2021-12-18 23:47:53 +0200

    feat: Notificatoins, account for user settings when sending them

--- app/plants/management/commands/sendnotifications.py
+++ app/plants/management/commands/sendnotifications.py
@@ -5,7 +5,8 @@ from plants.utils import get_time_between_watering_field_for_current_season
 from plants.models import Plant
 from users.models import PushSubscription, UserSettings
 from ._pushutils import send_to_subscription
-
+from datetime import date, datetime
+import pytz
 
 class Command(BaseCommand):
     help = 'Send notifications to users with plants which need to be watered today'
@@ -20,9 +21,20 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS('owner set:  "%s"' % owners_set))
         for owner in owners_set:
             try:
+                self.stdout.write(self.style.SUCCESS('trying to send notifications for user  "%s"' % owner))
                 subscription = PushSubscription.objects.get(user = owner)
                 settings = UserSettings.objects.get(user = owner)
-                payload = {"title": "Water me!", "message": "Some of your plants need to be watered!"}
-                send_to_subscription(subscription, payload)
+                now = timezone.now()
+                user_timezone = pytz.timezone(settings.timezone)
+                user_start_time = settings.notifications_start_time
+                user_end_time = settings.notifications_end_time
+                user_start_notifications_date = datetime(now.year, now.month, now.day, user_start_time.hour, user_start_time.minute, now.second, now.microsecond, user_timezone)
+                user_end_notifications_date = datetime(now.year, now.month, now.day, user_end_time.hour, user_end_time.minute, now.second, now.microsecond, user_timezone)
+                if now >= user_start_notifications_date and now <= user_end_notifications_date:
+                    self.stdout.write(self.style.SUCCESS('send notifications for user  "%s"' % owner))
+                    payload = {"title": "Water me!", "message": "Some of your plants need to be watered!"}
+                    send_to_subscription(subscription, payload)
             except PushSubscription.DoesNotExist:
-                self.stdout.write(self.style.SUCCESS('subscriptoin for user not exist  "%s"' % owner))
+                self.stdout.write(self.style.SUCCESS('subscriptoin for user does not exist  "%s"' % owner))
+            except UserSettings.DoesNotExist:
+                self.stdout.write(self.style.SUCCESS('UserSettings for user doesn not exist  "%s"' % owner))
