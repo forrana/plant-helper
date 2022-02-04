f08fcc5e5d6ce79526797b94ad40920656188aa0
Alex Forrana <forrana@gmail.com>
2022-02-04 22:34:19 +0200

    test: test that notifications are sent correctly in winter and summer times

--- app/plants/management/commands/tests.py
+++ app/plants/management/commands/tests.py
@@ -55,3 +55,27 @@ class NotificationsLogicTestCase(TestCase):
         )
         now = timezone.now().replace(hour=16, minute=0, second=1, microsecond=1)
         self.assertEqual(is_time_to_send_notification(now, current_interval, settings), False)
+
+    def test_notifications_are_sent_when_time_is_equal_in_the_summer(self):
+        """Server time is 6:00(UTC), start time is 8:00(+2)"""
+        current_interval = "15"
+        settings = UserSettings(
+            notifications_start_time = datetime.time(6, 0),
+            notifications_end_time = datetime.time(18, 0),
+            notifications_interval = current_interval,
+            timezone = "Europe/Helsinki"
+        )
+        now = timezone.now().replace(hour=8, minute=00, second=00, microsecond=00, month=6)
+        self.assertEqual(is_time_to_send_notification(now, current_interval, settings), True)
+
+    def test_notifications_are_sent_when_time_is_equal_in_the_winter(self):
+        """Server time is 6:00(UTC), start time is 8:00(+2)"""
+        current_interval = "15"
+        settings = UserSettings(
+            notifications_start_time = datetime.time(6, 0),
+            notifications_end_time = datetime.time(18, 0),
+            notifications_interval = current_interval,
+            timezone = "Europe/Helsinki"
+        )
+        now = timezone.now().replace(hour=8, minute=00, second=00, microsecond=00, month=1)
+        self.assertEqual(is_time_to_send_notification(now, current_interval, settings), True)
