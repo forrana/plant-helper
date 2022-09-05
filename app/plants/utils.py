d7d5ed37abb2b1c0cf9e6532fc39226f6c0b97f7
Alex Forrana <forrana@gmail.com>
2022-09-05 20:41:53 +0300

    feat: end growing season on 1t of September

--- app/plants/utils.py
+++ app/plants/utils.py
@@ -3,8 +3,8 @@ from datetime import date, datetime
 Y = 2000 # dummy leap year to allow input X-02-29 (leap day)
 seasons = [('winter', (date(Y,  1,  1),  date(Y,  3, 20))),
            ('spring', (date(Y,  3, 21),  date(Y,  6, 20))),
-           ('summer', (date(Y,  6, 21),  date(Y,  9, 22))),
-           ('autumn', (date(Y,  9, 23),  date(Y, 12, 20))),
+           ('summer', (date(Y,  6, 21),  date(Y,  9, 1))),
+           ('autumn', (date(Y,  9, 2),  date(Y, 12, 20))),
            ('winter', (date(Y, 12, 21),  date(Y, 12, 31)))]
 
 def get_season(now):
